f750e1cf8cdebeec25e5ad00d6ba14bcef86d8c7
multiverse22 <momizi527@gmail.com>
2024-08-01 16:36:38 +0900

    feat:종속관계구현

--- src/Data/StudentData.java
+++ src/Data/StudentData.java
@@ -3,13 +3,33 @@ package Data;
 import java.util.ArrayList;
 
 public class StudentData {
+    ScoreData scoreData;
+    SubjectData subjectData;
     int studentId;
     String studentName;
     ArrayList<String> subjectList = new ArrayList<>();
 
+    public StudentData(int studentId) {
+        this.studentId = studentId;
+        this.subjectData=new SubjectData();
+        this.scoreData=new ScoreData(studentId);
+    }
+    public void setScoreList(int score,String subjectType){
+        scoreData.setScoreList(score,subjectType);
+    }
+    public ArrayList<String> getGradeList(){
+        return scoreData.getGradeList();
+    }
+    public ArrayList<String> getScoreList(){
+        return scoreData.getScoreList();
+    }
+
+
+
     private int getStudentId() {
         return this.studentId;
     }
+
     private void setStudentId(int studentName) {
         this.studentId = studentName;
     }
