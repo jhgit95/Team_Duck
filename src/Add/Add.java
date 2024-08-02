5edf9e70cfce3b680d68300e90b2700c08e15aa9
unknown <lklll1212@gmail.com>
2024-08-02 10:57:20 +0900

    git commit minhyeok

--- src/Add/Add.java
+++ src/Add/Add.java
@@ -1,4 +1,16 @@
 package Add;
 
 public class Add {
+    int studentId;
+    int number =0;
+    String student;
+    String studentName;
+
+
+    public void setStudent(String studentname){
+        studentName = studentname;
+        studentId = number;
+        number++;
+    }
+
 }
